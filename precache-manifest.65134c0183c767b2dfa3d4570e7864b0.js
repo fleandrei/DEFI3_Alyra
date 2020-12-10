4dd9a3f7838c57381041a2d21615c465d0e5b382
3703599 <fleandrei@yahoo.com>
2020-12-10 14:31:07 +0100

    Updates

---
+++ precache-manifest.65134c0183c767b2dfa3d4570e7864b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa5a7f23cb13bcf91400b6c3519760d",
+    "url": "/DEFI3_Alyra/index.html"
+  },
+  {
+    "revision": "45f0307820d23703f156",
+    "url": "/DEFI3_Alyra/static/css/2.af317cf5.chunk.css"
+  },
+  {
+    "revision": "fb62baeff6a4ac08a7d1",
+    "url": "/DEFI3_Alyra/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "45f0307820d23703f156",
+    "url": "/DEFI3_Alyra/static/js/2.664fc12d.chunk.js"
+  },
+  {
+    "revision": "fb62baeff6a4ac08a7d1",
+    "url": "/DEFI3_Alyra/static/js/main.2b2500d0.chunk.js"
+  },
+  {
+    "revision": "32073859c262aae5c248",
+    "url": "/DEFI3_Alyra/static/js/runtime-main.e3ccb908.js"
+  }
+]);
